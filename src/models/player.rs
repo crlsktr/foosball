d9bc09a82b20c5b663d92fc652442a2313e144f9
Carlos Garcia <carlos.garcia@centeva.com>
2019-06-14 15:58:55 -0600

    hello there I can't think oof somethjing rright now ps: Not drunk

--- src/models/player.rs
+++ src/models/player.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 use super::super::schema::players;
 
-#[derive(Queryable, Serialize, Debug)]
+#[derive(Queryable, Serialize, Debug,Clone)]
 pub struct Player {
 	pub id: i32,
 	pub name: String,
