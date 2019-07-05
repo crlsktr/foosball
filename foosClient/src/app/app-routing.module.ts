a65571c94380834a176682ff039859dbbdef8f97
cadensorenson <caden.sorenson@centeva.com>
2019-07-05 14:50:49 -0600

    added few pages

--- foosClient/src/app/app-routing.module.ts
+++ foosClient/src/app/app-routing.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
 import {HomeModule} from './pages/home/home.module';
+import {LeaderboardComponent} from './pages/leaderboard/leaderboard.component';
+import {LeaderboardModule} from './pages/leaderboard/leaderboard.module';
+import {NewMatchComponent} from './pages/new-match/new-match.component';
+import {NewMatchModule} from './pages/new-match/new-match.module';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
-  {path: 'home', component: HomeComponent, }
+  {path: 'home', component: HomeComponent, },
+  {path: 'leaderboard', component: LeaderboardComponent},
+  {path: 'new/match', component: NewMatchComponent},
 ];
 
 @NgModule({
   imports: [
     HomeModule,
+    LeaderboardModule,
+    NewMatchModule,
     RouterModule.forRoot(routes)
   ],
   exports: [RouterModule]
