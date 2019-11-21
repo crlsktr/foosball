f0f14618d2b1057f1c3278c0162e1eef5ca861fc
Carlos Garcia <carlos.garcia@centeva.com>
2019-11-21 16:59:30 -0700

    stop me if you can

--- foosClient/src/app/pages/teamstats/teamstats.module.ts
+++ foosClient/src/app/pages/teamstats/teamstats.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TeamStatsComponent } from './teamstats.component';
 import { RouterModule } from '@angular/router';
+import { TimeSeriesModule } from 'src/app/components/time-series/time-series.module';
 
 @NgModule({
   declarations: [TeamStatsComponent],
   imports: [
     CommonModule,
     RouterModule,
+    TimeSeriesModule,
   ]
 })
 export class TeamStatsModule { }
